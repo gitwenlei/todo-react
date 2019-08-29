80f237dd800e7aaf6eff716327a6301e5bcc916e
gitwenlei <ngwenlei@gmail.com>
2019-08-29 20:46:20 +0800

    Renders the list

--- script.jsx
+++ script.jsx
@@ -1,3 +1,16 @@
+class ListItem extends React.Component {
+    render() {
+        console.log("the list: ", this.props.item);
+        return (
+            <li>{this.props.item}</li>
+            );
+    }
+}
+
+
+
+
+
 class List extends React.Component {
   constructor(){
     super()
@@ -15,7 +28,7 @@ class List extends React.Component {
     let currentList = this.state.list;
     currentList.push(this.state.word); //push new word into currentList
     this.setState({list: currentList}); //update state of list key to currentList
-    console.log("inside list liaoz", this.state.list);
+    // console.log("inside list liaoz", this.state.list);
   }
 
   changeHandler(){
@@ -25,15 +38,24 @@ class List extends React.Component {
     this.setState({word: currentValue});
   }
 
-  render() {
-      // render the list with a map() here
+    render() {
+        // render the list with a map() here
+        console.log("rendering");
 
-      console.log("rendering");
-      return (
+        console.log("inside the list: ", this.state.list);
+        // render each item from the list
+        let eachItem = this.state.list.map( (item, index) => {
+            return <ListItem item={item} key={index}></ListItem>;
+        });
+        console.log("each item:", eachItem);
+
+        return (
+
         <div className="container">
             <div className="input-group-lg">
                 <h1>to-do-list</h1>
                 <div className="list">
+                <ul>{eachItem}</ul>
                     <input onChange={()=>{this.changeHandler()}} value={this.state.word} className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
                     <button onClick={()=>{this.addItem()}}>add item</button>
                 </div>
