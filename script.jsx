21253db1df6167d1824ae34a2c53908ca663e52d
gitwenlei <ngwenlei@gmail.com>
2019-08-29 21:08:33 +0800

    created length validation of input

--- script.jsx
+++ script.jsx
@@ -17,6 +17,7 @@ class List extends React.Component {
 
     this.state = {
       word:"",
+      length: 0,
       list : []
     }
   }
@@ -28,26 +29,35 @@ class List extends React.Component {
     let currentList = this.state.list;
     currentList.push(this.state.word); //push new word into currentList
     this.setState({list: currentList}); //update state of list key to currentList
-    // console.log("inside list liaoz", this.state.list);
+    console.log("inside list liaoz", this.state.list);
   }
 
-  changeHandler(){
-    // debugger;
-    // console.log("TYPED: ", event.target.value);
-    let currentValue = event.target.value;
-    this.setState({word: currentValue});
-  }
+    changeHandler() {
+        // debugger;
+        // console.log("TYPED: ", event.target.value);
+        let currentValue = event.target.value;
+        let currentLength = event.target.value.length;
+
+        // input must less than 10 characters
+        if ( currentLength > 50) {
+            alert("keep it short!");
+
+        }
+
+        this.setState({length: currentLength});
+        this.setState({word: currentValue});
+
+    }
 
     render() {
         // render the list with a map() here
-        console.log("rendering");
+        // console.log("rendering");
 
-        console.log("inside the list: ", this.state.list);
         // render each item from the list
         let eachItem = this.state.list.map( (item, index) => {
             return <ListItem item={item} key={index}></ListItem>;
         });
-        console.log("each item:", eachItem);
+        // console.log("each item:", eachItem);
 
         return (
 
@@ -57,6 +67,7 @@ class List extends React.Component {
                 <div className="list">
                 <ul>{eachItem}</ul>
                     <input onChange={()=>{this.changeHandler()}} value={this.state.word} className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+                    <p>letters left: {this.state.length } / 50 </p>
                     <button onClick={()=>{this.addItem()}}>add item</button>
                 </div>
             </div>
